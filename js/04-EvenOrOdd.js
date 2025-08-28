d3710cfc1e24d03d084b0a624931966ec984c1b4
kirankumarblueray <kchallagiri@blueray.online>
2025-08-28 15:53:28 +0530

    even or odd

---
+++ js/04-EvenOrOdd.js
@@ -0,0 +1,9 @@
+function checkEvenOrOdd(number){
+    if(number % 2 == 0){
+        return "Even"
+    }
+    return "Odd"
+};
+
+console.log("5",checkEvenOrOdd(5));
+console.log("6",checkEvenOrOdd(6));
