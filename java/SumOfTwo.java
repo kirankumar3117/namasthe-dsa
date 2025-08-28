7df666fad6636bed076bf4d80aa15cac4e85ee43
kirankumarblueray <kchallagiri@blueray.online>
2025-08-28 15:08:52 +0530

    sum of two

---
+++ java/SumOfTwo.java
@@ -0,0 +1,10 @@
+public class SumOfTwo {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int result = sum(5, 10);
+        System.out.println("The sum is: " + result);
+    }
+}
---
+++ js/SumOfTwo.js
@@ -0,0 +1,7 @@
+function sumOfTwo(a, b) {
+    return a + b;
+}
+
+const add = sumOfTwo(7, 10);
+
+console.log(add)
