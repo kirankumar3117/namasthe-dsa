9f0c69cd64ebc32e8c7c680a65cd25d83e4a8a79
kirankumarblueray <kchallagiri@blueray.online>
2025-08-29 18:24:27 +0530

    right angle alternative ones and zeros

--- java/StarPatterns11.java
+++ java/StarPatterns11.java
@@ -1,10 +1,11 @@
 
-public class StarPatterns11{
+public class StarPatterns11 {
+
     // nxn pattern
-    public static void nxnStar(int n){
-        for(int i = 0; i < n; i++){
+    public static void nxnStar(int n) {
+        for (int i = 0; i < n; i++) {
             String row = "";
-            for(int j = 0; j < n; j++){
+            for (int j = 0; j < n; j++) {
                 row += "*";
             }
             System.out.println(row);
@@ -12,55 +13,55 @@ public class StarPatterns11{
     }
 
     //right angle star pattern 
-    public static void rightAngleStarPattern(int n){
-        for(int i=0; i<n;i++){
+    public static void rightAngleStarPattern(int n) {
+        for (int i = 0; i < n; i++) {
             String row = "";
-            for(int j=0;j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 row += "*";
             }
             System.out.println(row);
         }
     }
 
-     //right angle number pattern 
-    public static void rightAngleNumberPattern(int n){
-        for(int i=0; i<n;i++){
+    //right angle number pattern 
+    public static void rightAngleNumberPattern(int n) {
+        for (int i = 0; i < n; i++) {
             String row = "";
-            for(int j=0;j<=i;j++){
-                row += (j+1);
-            }
-            System.out.println(row);
-        }
-    }
-
-     //right triangle repeated number pattern 
-    public static void rightTrianleRepatedNummberPattern(int n){
-        for(int i=0; i<n;i++){
-            String row = "";
-            for(int j=0;j<=i;j++){
-                row += (i + 1);
-            }
-            System.out.println(row);
-        }
-    }
-
-    public static void reverseRightAngledTriangleIncreasingNumbers(int n){
-        for( int i=n;i >0;i--){
-            String row = "";
-            for(int j = 0; j < i; j++){
+            for (int j = 0; j <= i; j++) {
                 row += (j + 1);
             }
             System.out.println(row);
         }
     }
 
-    public static void rightAlignedRightAngledStarPattern(int n){
-        for(int i = 0; i < n ;i ++){
+    //right triangle repeated number pattern 
+    public static void rightTrianleRepatedNummberPattern(int n) {
+        for (int i = 0; i < n; i++) {
             String row = "";
-            for(int j = n-1;j>= 0; j--){
-                if(j<=i){
+            for (int j = 0; j <= i; j++) {
+                row += (i + 1);
+            }
+            System.out.println(row);
+        }
+    }
+
+    public static void reverseRightAngledTriangleIncreasingNumbers(int n) {
+        for (int i = n; i > 0; i--) {
+            String row = "";
+            for (int j = 0; j < i; j++) {
+                row += (j + 1);
+            }
+            System.out.println(row);
+        }
+    }
+
+    public static void rightAlignedRightAngledStarPattern(int n) {
+        for (int i = 0; i < n; i++) {
+            String row = "";
+            for (int j = n - 1; j >= 0; j--) {
+                if (j <= i) {
                     row += "*";
-                }else{
+                } else {
                     row += " ";
                 }
             }
@@ -68,12 +69,25 @@ public class StarPatterns11{
         }
     }
 
-    public static void main(String[] args){
+    public static void rightAngleAlternativeOnesAndZeros(int n) {
+        for (int i = 0; i < n; i++) {
+            String row = "";
+            int toggle = 1;
+            for (int j = 0; j <= i; j++) {
+                row += toggle;
+                toggle = (toggle == 1) ? 0 : 1;
+            }
+            System.out.println(row);
+        }
+    }
+
+    public static void main(String[] args) {
         // nxnStar(4);
         // rightAngleStarPattern(4);
         // rightAngleNumberPattern(4);
         // rightTrianleRepatedNummberPattern(4);
         // reverseRightAngledTriangleIncreasingNumbers(4);
-        rightAlignedRightAngledStarPattern(4);
+        // rightAlignedRightAngledStarPattern(4);
+        rightAngleAlternativeOnesAndZeros(4);
     }
 }
--- js/11-StarPatterns.js
+++ js/11-StarPatterns.js
@@ -64,9 +64,24 @@ const rightAlignedRightAngledStarPattern = (n) => {
   }
 };
 
+const rightAngleAlternativeOnesAndZeros = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+      if (j % 2 == 0) {
+        row += "1";
+      } else {
+        row += "0";
+      }
+    }
+    console.log(row);
+  }
+};
+
 // nxnStarPattern(4);
 // rightAngleStarPattern(4);
 // rightAngleNumberPattern(4);
 // rightTriangleRepeatedNumberPattern(4);
 // reverseRightAngledTriangleIncreasingNumbers(4);
-rightAlignedRightAngledStarPattern(4);
+// rightAlignedRightAngledStarPattern(4);
+rightAngleAlternativeOnesAndZeros(4);
