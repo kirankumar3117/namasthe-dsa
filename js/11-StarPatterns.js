a23e844d1f6092221e552dde038fecf9a6956f67
kirankumarblueray <kchallagiri@blueray.online>
2025-08-29 17:42:48 +0530

    right triangle repated number pattern

--- js/11-StarPatterns.js
+++ js/11-StarPatterns.js
@@ -30,6 +30,17 @@ const rightAngleNumberPattern = (n) => {
   }
 };
 
+const rightTriangleRepeatedNumberPattern = (n) => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+      row +=  (i+1)
+    }
+    console.log(row);
+  }
+};
+
 // nxnStarPattern(4);
 // rightAngleStarPattern(4);
 // rightAngleNumberPattern(4);
+rightTriangleRepeatedNumberPattern(4);
