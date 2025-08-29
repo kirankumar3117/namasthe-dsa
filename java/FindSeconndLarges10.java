b97255cc9787c4a5b0da4b2b2c5cde1b9cdec311
kirankumarblueray <kchallagiri@blueray.online>
2025-08-29 12:21:26 +0530

    finding second largets from an array

---
+++ java/FindSeconndLarges10.java
@@ -0,0 +1,27 @@
+
+public class FindSeconndLarges10 {
+
+    public static int secondLargest(int[] arr) {
+        if (arr.length < 2) {
+            return -1;
+        }
+        int firstLargest = arr[0] > arr[1] ? arr[0] : arr[1];
+        int secondLargets = arr[0] < arr[1] ? arr[0] : arr[1];
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] > secondLargets && arr[i] != firstLargest) {
+                secondLargets = arr[i];
+                if (firstLargest < secondLargets) {
+                    secondLargets = firstLargest;
+                    firstLargest = arr[i];
+                }
+            }
+        }
+        return secondLargets;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 7, 14, 2, 0, 9, 4, 14};
+        int result = secondLargest(arr);
+        System.out.println("Second Largest is: " + result);
+    }
+}
---
+++ js/10-FindingSecondLargest.js
@@ -0,0 +1,48 @@
+const arr = [1, 7, 14, 2, 0, 9, 4, 14];
+
+function FindSecondLargest(arr) {
+  if (arr.length < 2) {
+    return -1;
+  }
+
+  let firstLargest = arr[0] > arr[1] ? arr[0] : arr[1];
+  let secondLargest = arr[0] < arr[1] ? arr[0] : arr[1];
+
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] > secondLargest && arr[i] != firstLargest) {
+      secondLargest = arr[i];
+      if (firstLargest < secondLargest) {
+        secondLargest = firstLargest;
+        firstLargest = arr[i];
+      }
+    }
+  }
+
+  return secondLargest;
+}
+
+/* Corner Cases */
+
+// 1. what if array size is less than two, what should i return
+// 2. is array contains negative integers as well ?
+// 3. does array contains duplicates as well
+
+//infinite way
+// function FindSecondLargest(arr) {
+//   let firstLargest = -Infinity;
+//   let secondLargest = -Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (secondLargest < arr[i]) {
+//       secondLargest = arr[i];
+//       if (firstLargest < secondLargest) {
+//         secondLargest = firstLargest;
+//         firstLargest = arr[i];
+//       }
+//     }
+//   }
+
+//   return secondLargest;
+// }
+
+console.log("second largest", FindSecondLargest(arr));
